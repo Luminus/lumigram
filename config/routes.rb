a35d6b631304bce65fe65dd3166a4bd5b73bf69d
Olumide Alabi <luminus@vedutanova.com>
2015-12-28 17:10:21 +0100

    Setup Post & Comment Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/show'
+
+  get 'comments/edit'
+
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
   devise_for :users
   root 'posts#index'
 end
