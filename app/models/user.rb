ab0e8700bbaecd8b20b82e578f78f4c4c841333c
Olumide Alabi <luminus@vedutanova.com>
2015-12-28 18:04:28 +0100

    Prettify Devise Views

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_presence_of :username
+  validates_presence_of :avatar
 end
