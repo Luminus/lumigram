48b933ff51ef82a6c8ccd28ede20cedad57c02a6
Olumide Alabi <luminus@vedutanova.com>
2015-12-29 08:20:23 +0100

    Finish Ability to Add and display comments on each Post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,21 @@
 class CommentsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_post, only: [:new, :create]
+  before_action :set_comment, only: [:destroy]
 
   def new
+    @comment = @post.comments.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @post, notice: 'Comment successfully added'
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -10,4 +23,23 @@ class CommentsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    @post = @comment.post
+    @comment.destroy
+    redirect_to @post, notice: 'Comment deleted successfully'
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
 end
